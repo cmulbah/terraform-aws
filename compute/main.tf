99c35cc95efd064c9332fe5eca0a55de939f2c82
Troy Ingram <ubuntu@ip-172-31-18-124.ec2.internal>
2021-06-17 13:53:52 +0000

    update

--- compute/main.tf
+++ compute/main.tf
@@ -14,13 +14,13 @@ resource "random_id" "mtc_node_id" {
   byte_length = 2
   count       = var.instance_count
   keepers = {
-      key_name = var.key_name
+    key_name = var.key_name
   }
 }
 
 resource "aws_key_pair" "mtc_auth" {
-    key_name = var.key_name
-    public_key = file(var.public_key_path)
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
 }
 
 resource "aws_instance" "mtc_node" {
@@ -31,10 +31,49 @@ resource "aws_instance" "mtc_node" {
     Name = "mtc_node-${random_id.mtc_node_id[count.index].dec}"
   }
 
-  key_name = aws_key_pair.mtc_auth.id
+  key_name               = aws_key_pair.mtc_auth.id
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnets[count.index]
+  user_data = templatefile(var.user_data_path,
+    {
+      nodename    = "mtc-${random_id.mtc_node_id[count.index].dec}"
+      db_endpoint = var.db_endpoint
+      dbuser      = var.dbuser
+      dbname      = var.dbname
+      dbpass      = var.dbpassword
+    }
+  )
   root_block_device {
     volume_size = var.vol_size #10
   }
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = self.public_ip
+      private_key = file(var.private_key_path)
+    }
+    script = "${path.cwd}/delay.sh"
+  }
+  provisioner "local-exec" {
+    command = templatefile("${path.cwd}/scp_script.tpl",
+      {
+        nodeip   = self.public_ip
+        k3s_path = "${path.cwd}/../"
+        nodename = self.tags.Name
+        private_key_path = var.private_key_path
+      }
+    )
+  }
+  provisioner "local-exec" {
+    when = destroy
+    command = "rm -f ${path.cwd}/../k3s-${self.tags.Name}.yaml"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "mtc_tg_attach" {
+  count            = var.instance_count
+  target_group_arn = var.lb_target_group_arn
+  target_id        = aws_instance.mtc_node[count.index].id
+  port             = var.tg_port
 }
--- compute/variables.tf
+++ compute/variables.tf
@@ -7,3 +7,12 @@ variable "public_subnets" {}
 variable "vol_size" {}
 variable "key_name" {}
 variable "public_key_path" {}
+variable "dbuser" {}
+variable "dbname" {}
+variable "dbpassword" {}
+variable "db_endpoint" {}
+variable "user_data_path" {}
+variable "lb_target_group_arn" {}
+variable "tg_port" {}
+variable "private_key_path" {}
+
